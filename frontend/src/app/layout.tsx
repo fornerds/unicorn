33f5f4d86a7a9f998a33036ac5300e10f04fbc5e
KANGPUNGYUN <zkdvnd@naver.com>
2026-03-05 06:49:48 +0900

    feat: 폰트 에러 수정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Cardo } from 'next/font/google';
+import localFont from 'next/font/local';
 import '../styles/globals.css';
 import { Providers } from './providers';
 import { LayoutWrapper } from './layout-wrapper';
@@ -8,6 +9,28 @@ const cardo = Cardo({
   weight: ['400', '700'],
   subsets: ['latin'],
   variable: '--font-cardo',
+  display: 'swap',
+});
+
+const elice = localFont({
+  src: [
+    {
+      path: '../../public/fonts/EliceDXNeolliOTF-Light.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/EliceDXNeolliOTF-Medium.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../../public/fonts/EliceDXNeolliOTF-Bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-elice',
   display: 'swap',
 });
 
@@ -29,7 +52,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="ko" className={cardo.variable}>
+    <html lang="ko" className={`${cardo.variable} ${elice.variable}`}>
       <body className="font-suit">
         <Providers>
           <LayoutWrapper>{children}</LayoutWrapper>
